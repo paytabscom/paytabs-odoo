295dbdffcbf9c0a85d0bd79adae5bf5f23ad7ff9
Wajih <wajih.km@gmail.com>
2026-09-17 09:02:18 +0000

    Update the main screenshot

--- payment_paytabs_official/__manifest__.py
+++ payment_paytabs_official/__manifest__.py
@@ -8,7 +8,7 @@
     'summary': "A payment provider covering the Middle East and North Africa.",
     'description': " ",  # Non-empty string to avoid loading the README file.
     'depends': ['payment'],
-    'images': ['static/description/main_screenshot.gif'],
+    'images': ['static/description/paytabs-strapline.png'],
     'data': [
         'views/payment_provider_views.xml',
         'views/payment_paytabs_official_templates.xml',
